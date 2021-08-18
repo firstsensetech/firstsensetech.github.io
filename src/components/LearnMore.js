64151edc55051ac7f0fa15c95581d5dde0a0b06c
aniruddhparwal <aniruddhparwal@gmail.com>
2021-08-18 23:49:11 +0530

    Learn More animation added

--- src/components/LearnMore.js
+++ src/components/LearnMore.js
@@ -13,19 +13,19 @@ function LearnMore() {
             <div className="learnMore__datils">
                 {/* <h1>Our Focus</h1> */}
                 <div className="learnMore__details__block" id="first">
-                    <div id="content">
+                    <div id="content" data-aos="fade-left">
                         <h3>EduTech</h3>
                         <p>Hybrid Practoring Class Dynamics</p>
                     </div>
-                    <div id="image">
+                    <div id="image" data-aos="fade-right">
                         <img src={school} />
                     </div>
                 </div>
                 <div className="learnMore__details__block" id="second">
-                    <div id="image">
+                    <div id="image" data-aos="fade-left">
                         <img src={healthcare} />
                     </div>
-                    <div id="content">
+                    <div id="content" data-aos="fade-right">
                         <h3>Health Care</h3>
                         <p>Fall detection Patient Monitoring Sports Medicine</p>
                     </div>
