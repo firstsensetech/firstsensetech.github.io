c444ee39ca65f2ad14c10688a3ad0360bfa8178e
aniruddhparwal <aniruddhparwal@gmail.com>
2021-08-19 11:49:49 +0530

    Nav bar updated

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,6 +1,8 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 import fsMainLogo from '../Images/MainLogo.svg'
+import ContactSupportIcon from '@material-ui/icons/ContactSupport';
+
 
 function NavBar() {
     return (
@@ -9,6 +11,7 @@ function NavBar() {
                 <Link to="/">
                     <img src={fsMainLogo} />
                 </Link>
+                <a href="#contact_us"><ContactSupportIcon /></a>
             </div>
             <div className="navbar__right">
                 {/* <span>About Us</span>
